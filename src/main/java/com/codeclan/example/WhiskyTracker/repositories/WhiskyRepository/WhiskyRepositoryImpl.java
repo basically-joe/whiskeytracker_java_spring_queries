2990303a878bf8ad543358adeff42b1c92bdc57a
Joe McColl <joe4kool@gmail.com>
2019-02-27 19:38:00 +0000

    added whiskey by year REST route, displays empty array in browser

--- src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
+++ src/main/java/com/codeclan/example/WhiskyTracker/repositories/WhiskyRepository/WhiskyRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
 
 import com.codeclan.example.WhiskyTracker.models.Whisky;
 import org.hibernate.Criteria;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.criterion.Restrictions;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,11 +23,15 @@ public class WhiskyRepositoryImpl implements WhiskyRepositoryCustom {
 
 		Session session = entityManager.unwrap(Session.class);
 
-		Criteria cr = session.createCriteria(Whisky.class);
-		cr.createAlias("distillery", "distilleryAlias");
-		cr.add(Restrictions.eq("distilleryAlias.id", id));
-		result = cr.list();
-
+		try {
+			Criteria cr = session.createCriteria(Whisky.class);
+			cr.createAlias("distillery", "distilleryAlias");
+			cr.add(Restrictions.eq("distilleryAlias.id", id));
+			result = cr.list();
+		}
+		catch (HibernateException ex){
+			ex.printStackTrace();;
+		}
 		return result;
 	}
 
